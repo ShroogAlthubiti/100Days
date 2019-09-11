e0b12ba8cb7497ffdc4b1e3ecdbde26aab7af1bc
Shroog <shroog.1997s@gmail.com>
2019-09-11 19:54:13 +0300

    24 Day

---
+++ 24.py
@@ -0,0 +1,20 @@
+
+thedict=dict(name= "shroog", age= 22 , study= "computer science")
+
+mydict1 = thedict.copy()
+mydict2 = dict(thedict)
+
+print(mydict1)
+print(mydict2)
+print(mydict2.keys())
+
+child3 = {"name":"Linus" , "year":2011}
+myfamily={
+	"child1":{"name":"Emil" , "year":2004},
+	"child2":{"name":"Tobias" , "year":2007},
+	"child3":child3
+ }
+ 
+print(myfamily)
+
+
