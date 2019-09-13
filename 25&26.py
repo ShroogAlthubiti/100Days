03b2d5b82597bc8d90a0a92bff531b44c8603adb
Shroog <shroog.1997s@gmail.com>
2019-09-13 21:34:05 +0300

    25&26 Days

---
+++ 25&26.py
@@ -0,0 +1,17 @@
+# Q1:
+set1={1,3,5,7,8}
+print(set1)
+set1.add(4)
+set1.add(8)
+set1.add(12)
+print(set1)
+set1.remove(8)
+print(set1)
+set1.clear()
+print(set1)
+#Q2:
+dic=dict(name="pigeon",type="bird",skin_cover = "feathers" )
+
+print("Type: ",dic['type'])
+dic['skin_cover']= "none"
+print(dic)
