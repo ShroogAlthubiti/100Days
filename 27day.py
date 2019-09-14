1720d7c09d5476903651b9b6d32f2eefb0980f16
Shroog <shroog.1997s@gmail.com>
2019-09-15 00:24:46 +0300

    27 Days

---
+++ 27day.py
@@ -0,0 +1,21 @@
+a=10
+b=12
+c=10
+if a>b and a==c :
+	print("a greater than b and equle c")
+elif a<b or b>c:
+	print("a less than b and b greater than c")
+else :
+	print("a equle b")
+c=81
+print("A") if c > 90 else print("B") if c > 80 else print("C")
+
+x=4
+if x>0:
+	print("positive number")
+	if x%2==0:
+		print("even number")
+	else:
+		print("odd number")
+
+
