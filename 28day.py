3387b12e75e33fd935bae9384594b7a42f2d79f4
Shroog <shroog.1997s@gmail.com>
2019-09-15 19:08:45 +0300

    28 Days

---
+++ 28day.py
@@ -0,0 +1,29 @@
+
+i =1
+while i<10:
+	print(i , end="  ")
+	i+=1
+else:
+	print("\nend loop")
+x=0
+    ########
+while x<10:
+	print(x , end= "  ")
+	if x==6:
+		break
+	x+=2
+	###########
+print()
+y=0
+while y<10:
+	y+=1
+	if y %2==0:
+		continue
+	print(y , end= "  ")
+print()
+	
+
+
+
+
+
