1c07584c316bf19388b47a5871ca7ce26fcbf6ac
Shroog <shroog.1997s@gmail.com>
2019-09-17 23:08:03 +0300

    30 Days

---
+++ 30.py
@@ -0,0 +1,15 @@
+for x in range(5):
+	print(x , end= ' ')
+print("\n")
+for x in range(2,5):
+	print(x , end=' ')
+else: # use else 
+	print("\nEnd")
+for x in range(0,30,5):
+	print(x,end=' ')
+print("\n")
+#Nested Loop:
+for x in range(3):
+	for y in range(3):
+		print(x,y)
+
