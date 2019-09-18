a0531fdee14e9e37ec7b335c81f43a9861e4dccf
Shroog <shroog.1997s@gmail.com>
2019-09-18 20:17:30 +0300

    31 Days

---
+++ 31.py
@@ -0,0 +1,12 @@
+#User-Defined Function
+def my_function (name, country="KSA"):#Default Parameter value 
+	print("My name: ", name,", from: ", country)
+
+# Calling a Function
+my_function("Shroog")
+
+my_function("jonh","USA")
+
+my_function("Lee","Korea")
+
+my_function("Ali")
