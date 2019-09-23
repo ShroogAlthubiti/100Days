efdcad5984c6199d0bb0b758f7c600b7f222947c
Shroog <shroog.1997s@gmail.com>
2019-09-23 19:22:24 +0300

    34 Days

---
+++ 34day.py
@@ -0,0 +1,28 @@
+def my_function (name):
+	for x in name:
+		print(x)
+name=["Shroog", "Ali", "Ahmad"]
+my_function(name)
+
+def muliply (num):
+	return 5*num
+print(muliply(5))
+print(muliply(6))
+
+def child(child1,child2,child3):
+	print("the youngest child is",child3)
+child(child1="Emil",child2="jonh",child3="aliee")
+
+def child1(*Kids):
+	print("the youngest child is",Kids[2])
+child1("Emil","aliee","jonh")
+
+def recursion(K):
+	if(K>0):
+		result=K+recursion(K-1)
+		print(result)
+	else:
+		result=0
+	return result
+print("Recursion Example Result")
+recursion(4)
