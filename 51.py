494300ce3e023814b4d900caace55cf774b7d010
Shroog <shroog.1997s@gmail.com>
2019-10-09 21:24:38 +0300

    51 Days

---
+++ 51.py
@@ -0,0 +1,15 @@
+
+
+import mymodule  as md
+import platform 
+
+
+print(platform.system())
+print(platform.python_version())
+print(dir(platform))
+
+md.greeting("Shroog")
+
+a=md.person['age']
+print(a)
+
